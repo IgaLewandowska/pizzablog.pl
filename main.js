fa00639298f4e270c9832aea8378e8d64d85e9ee
IgaLewandowska <igalewandowska98@wp.pl>
2022-10-05 19:07:10 +0200

    11.5

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle('dark-mode');
+    let body = document.body;
+    let header = document.getElementById('header-id');
+    let list_class = body.className.split(' ');
+    if (list_class.includes('dark-mode')) {
+        delete_class(header, 'dark-mode');
+        delete_class(body, 'dark-mode');
+    } else {
+        add_class(body, 'dark-mode');
+        add_class(header, 'dark-mode');
+    }
+}
+
+function add_class(element, class_name) {
+    element.className = class_name;
+}
+
+function delete_class(element, class_name) {
+    element.classList.remove(class_name);
 }

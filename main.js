c7580a90e1a436e751f0847c8186f8a28c668d99
IgaLewandowska <igalewandowska98@wp.pl>
2022-10-10 20:57:41 +0200

    11.8

--- main.js
+++ main.js
@@ -2,15 +2,26 @@ function myFunction() {
     let body = document.body;
     let header = document.getElementById('header-id');
     let footer = document.getElementById('footer-id');
+    let links = document.getElementsByClassName('links');
     let list_class = body.className.split(' ');
+    // let links_dark = '';
     if (list_class.includes('dark-mode')) {
         delete_class(header, 'dark-mode');
         delete_class(body, 'dark-mode');
         delete_class(footer, 'dark-mode');
+        for(let i = 0; i<links.length; i++) {
+            console.log(links[i]);
+            delete_class(links[i], 'dark-mode');
+            // names += links[i].name;
+            // links_dark += delete_class(links, 'dark-mode');
+        }
     } else {
         add_class(body, 'dark-mode');
         add_class(header, 'dark-mode');
         add_class(footer, 'dark-mode');
+        for(let i = 0; i<links.length; i++) {
+            add_class(links[i], 'dark-mode');
+        }
     }
 }
 
